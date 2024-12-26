b3d9ac3f89bcb7f7b9d145d4a83b91a9381507cc
Hudhaifa Al-obaidi <hozifa.alobidy@gmail.com>
2024-12-26 21:35:41 +0300

    final commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,6 @@
 const clientID ="put your client ID here";
 const clientSecret = "put your client Secret here";
-const dataUrl = "https://graph.microsoft.com/v1.0/users/HudhaifaAbdulhameed@alsalam055.onmicrosoft.com/todo/lists/AAMkAGJlOGE0Mzg4LTdlZDEtNDg5My1hNzQxLTUzY2MzYWRiNjk5YwAuAAAAAAAU8M7wXlh9T58AJCUyw8kIAQAY4XxknhslRpy3ZEUbJdarAAAAAAESAAA=/tasks";
-const tokenUrl ="https://login.microsoftonline.com/c17977d2-9fad-4c2e-82a9-5304e49f32b3/oauth2/v2.0/token";
+const userPrincipalName = "put your userPrincipalName here";
+const tenantID = "put your tenant ID here";
+const dataUrl = "https://graph.microsoft.com/v1.0/users/$userPrincipalName/todo/lists/AAMkAGJlOGE0Mzg4LTdlZDEtNDg5My1hNzQxLTUzY2MzYWRiNjk5YwAuAAAAAAAU8M7wXlh9T58AJCUyw8kIAQAY4XxknhslRpy3ZEUbJdarAAAAAAESAAA=/tasks";
+const tokenUrl ="https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token";
